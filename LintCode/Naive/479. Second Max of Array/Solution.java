d6c02a6cb1dc762dabd929d6b94060718133fd8a
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-06-16 23:14:01 -0500

    479. Second Max of Array

--- LintCode/Naive/479. Second Max of Array/Solution.java
+++ LintCode/Naive/479. Second Max of Array/Solution.java
@@ -6,10 +6,10 @@ public class Solution {
     public int secondMax(int[] nums) {
         // write your code here
         if (nums.length == 1) return nums[0];
-        
+
         int max = Integer.MIN_VALUE;
         int secondmax = Integer.MIN_VALUE;
-        
+
         for (int num : nums) {
             if (num > max) {
                 secondmax = max;
@@ -33,7 +33,7 @@ public class Solution {
     public int secondMax(int[] nums) {
         // write your code here
         if (nums.length == 1) return nums[0];
-        
+
         for (int i = 0; i < nums.length; i++) {
             int min = nums[i];
             int pos = i;
@@ -43,13 +43,13 @@ public class Solution {
                     pos = j;
                 }
             }
-            
+
             int tmp = nums[i];
             nums[i] = nums[pos];
             nums[pos] = tmp;
-            
+
         }
-        
+
         return nums[nums.length - 2];
     }
 }
