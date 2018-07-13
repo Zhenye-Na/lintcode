7f5c7403313df3d12d92e0888f17218260cb4e53
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-07-12 23:51:28 -0500

    Recursion

--- LintCode/Medium/17. Subsets/Solution.java
+++ LintCode/Medium/17. Subsets/Solution.java
@@ -25,3 +25,45 @@ public class Solution {
     }
     
 }
+
+
+
+/**
+* 本参考程序来自九章算法，由 @九章管理员 提供。版权所有，转发请注明出处。
+* - 九章算法致力于帮助更多中国人找到好的工作，教师团队均来自硅谷和国内的一线大公司在职工程师。
+* - 现有的面试培训课程包括：九章算法班，系统设计班，算法强化班，Java入门与基础算法班，Android 项目实战班，
+* - Big Data 项目实战班，算法面试高频题班, 动态规划专题班
+* - 更多详情请见官方网站：http://www.jiuzhang.com/?source=code
+*/ 
+
+// Non Recursion
+class Solution {
+    /**
+     * @param S: A set of numbers.
+     * @return: A list of lists. All valid subsets.
+     */
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        int n = nums.length;
+        Arrays.sort(nums);
+        
+        // 1 << n is 2^n
+        // each subset equals to an binary integer between 0 .. 2^n - 1
+        // 0 -> 000 -> []
+        // 1 -> 001 -> [1]
+        // 2 -> 010 -> [2]
+        // ..
+        // 7 -> 111 -> [1,2,3]
+        for (int i = 0; i < (1 << n); i++) {
+            List<Integer> subset = new ArrayList<Integer>();
+            for (int j = 0; j < n; j++) {
+                // check whether the jth digit in i's binary representation is 1
+                if ((i & (1 << j)) != 0) {
+                    subset.add(nums[j]);
+                }
+            }
+            result.add(subset);
+        }
+        return result;
+    }
+}
