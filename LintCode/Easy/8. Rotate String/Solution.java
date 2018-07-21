933565a7d63a204ac36fca61c124ed811f3b6a04
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-07-20 23:46:55 -0500

    Two Pointers

--- LintCode/Easy/8. Rotate String/Solution.java
+++ LintCode/Easy/8. Rotate String/Solution.java
@@ -33,3 +33,35 @@ public class Solution {
 
     }
 }
+
+
+
+
+
+public class Solution {
+    /**
+     * @param str: An array of char
+     * @param offset: An integer
+     * @return: nothing
+     */
+    public void rotateString(char[] str, int offset) {
+        // write your code here
+        if (str == null || str.length == 0 || offset == 0) {
+            return;
+        }
+
+        int length = str.length;
+        offset = offset % length;
+        reverse(str, 0, length - offset - 1);
+        reverse(str, length - offset, length - 1);
+        reverse(str, 0, length - 1);
+    }
+    
+    private void reverse(char[] str, int start, int end) {
+        for (int i = start, j = end; i < j; i++, j--) {
+            char temp = str[i];
+            str[i] = str[j];
+            str[j] = temp;
+        }
+    }
+}
