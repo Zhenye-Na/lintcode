979b8a79f898a9e27a2dc076f0fe5b2178ca9f18
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-08-17 02:19:04 -0500

    427. Generate Parentheses | Backtracking

---
+++ LintCode/Medium/427. Generate Parentheses/README.md
@@ -0,0 +1,9 @@
+427. Generate Parentheses
+Description
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+Have you met this question in a real interview?  
+Example
+Given n = 3, a solution set is:
+
+"((()))", "(()())", "(())()", "()(())", "()()()"
---
+++ LintCode/Medium/427. Generate Parentheses/Solution.java
@@ -0,0 +1,38 @@
+public class Solution {
+    /**
+     * @param n: n pairs
+     * @return: All combinations of well-formed parentheses
+     */
+    public List<String> generateParenthesis(int n) {
+        // write your code here
+        List<String> result = new ArrayList<>();
+        if (n <= 0) return result;
+        helper(result, "", n, n);
+        return result;
+    }
+
+    private static void helper(List<String> result, String chosen, int left, int right) {
+        // If remaining left is greater than right -> # ")" is greater than #"(", return
+        if (left > right) {
+            return;
+        }
+
+        // Remaining "(" and ")" are 0 -> valid pairs, return
+        if (left == 0 && right == 0) {
+            result.add(chosen);
+            return;
+        }
+
+        // add "("
+        if (left > 0) {
+            helper(result, chosen + "(", left - 1, right);
+        }
+
+        // add ")"
+        if (right > 0) {
+            helper(result, chosen + ")", left, right - 1);
+        }
+    }
+
+
+}
