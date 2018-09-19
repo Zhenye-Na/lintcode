955ac0308f34b782013a7c4ed1df3ebb4952ed4d
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-09-19 00:56:01 -0500

    57. 3Sum

--- 3. Medium/57. 3Sum/Solution.py
+++ 3. Medium/57. 3Sum/Solution.py
@@ -36,3 +36,45 @@ class Solution:
                         end -= 1
 
         return result
+
+
+
+
+class Solution:
+    """
+    @param nums: Give an array numbers of n integer
+    @return: Find all unique triplets in the array which gives the sum of zero.
+    """
+    def threeSum(self, nums):
+        # write your code here
+        nums.sort()
+        results = []
+        length  = len(nums)
+
+        # permutate the first number
+        for i in range(0, length - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            # 2Sum == target
+            target = -nums[i]
+
+            # Two Pointers
+            left, right = i + 1, length - 1
+            while left < right:
+                if nums[left] + nums[right] == target:
+                    results.append([nums[i], nums[left], nums[right]])
+                    right -= 1
+                    left += 1
+
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+                elif nums[left] + nums[right] > target:
+                    right -= 1
+                else:
+                    left += 1
+
+        return results
