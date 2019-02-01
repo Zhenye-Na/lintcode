cffdfa63987425ce2d09d6c533d9b6456dbaf313
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2019-01-31 19:14:16 -0600

    384. Longest Substring Without Repeating Characters

--- 3. Medium/384. Longest Substring Without Repeating Characters/Solution.go
+++ 3. Medium/384. Longest Substring Without Repeating Characters/Solution.go
@@ -2,20 +2,38 @@
  * @param s: a string
  * @return: an integer
  */
- func lengthOfLongestSubstring (s string) int {
-    // write your code here
-    lastOccurred := make(map[byte]int)
-    start := 0
-    maxLength := 0
+func lengthOfLongestSubstring(s string) int {
+	// write your code here
+	lastOccurred := make(map[byte]int)
+	start := 0
+	maxLength := 0
 
-    for idx, ch := range []byte(s) {
-        if lastIdx, err := lastOccurred[ch]; err && lastIdx >= start {
-            start = lastIdx + 1
-        }
-        if idx - start + 1 > maxLength {
-            maxLength = idx - start + 1
-        }
-        lastOccurred[ch] = idx
-    }
-    return maxLength
+	for idx, ch := range []byte(s) {
+		if lastIdx, ok := lastOccurred[ch]; ok && lastIdx >= start {
+			start = lastIdx + 1
+		}
+		if idx-start+1 > maxLength {
+			maxLength = idx - start + 1
+		}
+		lastOccurred[ch] = idx
+	}
+	return maxLength
+}
+
+func lengthOfLongestSubstring2(s string) int {
+	// write your code here
+	lastOccurred := make([]int, 0xffff)
+	start := 0
+	maxLength := 0
+
+	for idx, ch := range []byte(s) {
+		if lastIdx := lastOccurred[ch]; lastIdx > start {
+			start = lastIdx
+		}
+		if idx-start+1 > maxLength {
+			maxLength = idx - start + 1
+		}
+		lastOccurred[ch] = idx + 1
+	}
+	return maxLength
 }
