89195262f78da1946654318aea2d2f7978f00514
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2019-06-23 23:21:25 -0500

    :white_check_mark: Stack

---
+++ Stack/1116.Exclusive Time of Functions/Solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    """
+    @param n: a integer
+    @param logs: a list of integers
+    @return: return a list of integers
+    """
+    def exclusiveTime(self, n, logs):
+        # write your code here
+        stack = []
+        result = [0 for _ in range(n)]
+        last_timestamp = 0
+
+        for log in logs:
+            log = log.split(":")
+            thread_id, status, timestamp = int(log[0]), log[1], int(log[2])
+
+            if status == "start":
+                if stack:
+                    result[stack[-1]] += timestamp - last_timestamp
+                stack.append(thread_id)
+            else:
+                timestamp += 1
+                result[stack.pop()] += timestamp - last_timestamp
+
+            last_timestamp = timestamp 
+
+        return result
