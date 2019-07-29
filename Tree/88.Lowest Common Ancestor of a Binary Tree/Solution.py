639601259305e4918677b1ef88b76ac19a41bd10
Na, Zhenye <nazhenye@gmail.com>
2019-07-29 16:07:57 -0700

    :white_check_mark: LCA

--- Tree/88.Lowest Common Ancestor of a Binary Tree/Solution.py
+++ Tree/88.Lowest Common Ancestor of a Binary Tree/Solution.py
@@ -6,7 +6,6 @@ class TreeNode:
         self.left, self.right = None, None
 """
 
-
 class Solution:
     """
     @param: root: The root of the binary search tree.
@@ -14,6 +13,7 @@ class Solution:
     @param: B: A TreeNode in a Binary.
     @return: Return the least common ancestor(LCA) of the two nodes.
     """
+
     def lowestCommonAncestor(self, root, A, B):
         # write your code here
         node = self._dfs(root, A, B)
@@ -29,18 +29,17 @@ class Solution:
         left = self._dfs(root.left, A, B)
         right = self._dfs(root.right, A, B)
 
-
         # A 和 B 一边一个
-        if left and right: 
+        if left and right:
             return root
-        
+
         # 左子树有一个点或者左子树有 LCA
         if left:
             return left
-        
+
         # 右子树有一个点或者右子树有 LCA
         if right:
             return right
-        
+
         # 左右子树啥都没有
         return None
