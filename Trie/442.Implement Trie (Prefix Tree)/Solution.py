1982190981c48d54eca5b9026e023cf174a2c434
Na, Zhenye <nazhenye@gmail.com>
2020-01-05 17:47:47 -0800

    :white_check_mark: Trie | Prefix Tree

--- Trie/442.Implement Trie (Prefix Tree)/Solution.py
+++ Trie/442.Implement Trie (Prefix Tree)/Solution.py
@@ -1,22 +1,21 @@
 class TrieNode:
-    
+
     def __init__(self):
         self.children = {}
         self.is_word = False
 
 
-
 class Trie:
-    
+
     def __init__(self):
         # do intialization if necessary
         self.root = TrieNode()
-
 
     """
     @param: word: a word
     @return: nothing
     """
+
     def insert(self, word):
         # write your code here
         node = self.root
@@ -27,10 +26,10 @@ class Trie:
 
         node.is_word = True
 
-
     """
     return the node in the trie if exists 
     """
+
     def find(self, word):
         node = self.root
         for c in word:
@@ -39,21 +38,21 @@ class Trie:
                 return None
         return node
 
-
     """
     @param: word: A string
     @return: if the word is in the trie.
     """
+
     def search(self, word):
         # write your code here
         node = self.find(word)
         return node is not None and node.is_word
 
-
     """
     @param: prefix: A string
     @return: if there is any word in the trie that starts with the given prefix.
     """
+
     def startsWith(self, prefix):
         # write your code here
         return self.find(prefix) is not None
